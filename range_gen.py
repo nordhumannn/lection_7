193427197c5d3eeee8490e34554fb99b52c8aa0f
nordhumannn <97041147+nordhumannn@users.noreply.github.com>
2022-07-24 20:45:34 +0300

    Update range_gen.py

--- range_gen.py
+++ range_gen.py
@@ -1,4 +1,6 @@
 def range_gen(start=None, stop=None, step=None):
+    if not isinstance(start, int | None) or not isinstance(stop, int | None) or not isinstance(step, int | None):
+        raise TypeError()
     if step is None:
         step = 1
     if not start is None and stop is None:
@@ -12,3 +14,6 @@ def range_gen(start=None, stop=None, step=None):
             yield start
             start += step
     return
+
+for item in range_gen(1, 's'):
+    print(item)
