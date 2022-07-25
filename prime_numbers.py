de6467a0a215a52ef294e79923c9148228bfc977
nordhumannn <97041147+nordhumannn@users.noreply.github.com>
2022-07-25 16:54:53 +0300

    Update prime_numbers.py

--- prime_numbers.py
+++ prime_numbers.py
@@ -14,3 +14,13 @@ def prime_nums(n):
 prime_nums = list(item for item in prime_nums(100))
 
 print(prime_nums)
+
+#-----------------------------
+
+def prime_nums(stop):
+    for n in range(1, stop):
+        for i in range(2, n):
+            if not n % i:
+                break
+        else:
+            yield n
